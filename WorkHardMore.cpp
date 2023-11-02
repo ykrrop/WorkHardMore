81053ce8435d7de0897101f843cee7c6411a024e
Kirill Kropotin <krirokrop2018@gmail.com>
2023-11-02 21:24:29 +0300

    sortvubor

--- WorkHardMore.cpp
+++ WorkHardMore.cpp
@@ -3,36 +3,32 @@
 #include <string>
 using namespace std;
 
-//бинарный поиск
-int binarySearch(vector <int> arr, int left, int right, int key) {
-	while (left <= right) {
-		int mid = left + (right - left) / 2;
-		if (arr[mid] == key) {
-			return mid; // Искомый элемент найден, возвращаем его индекс
-		}
-		else if (arr[mid] < key) {
-			left = mid + 1; // Искомый элемент находится справа от mid
-		}
-		else {
-			right = mid - 1; // Искомый элемент находится слева от mid
+
+int main()
+{
+	const int N = 10;
+	int a[N] = { 1, 25, 6, 32, 43, 5, 96, 23, 4, 55 };
+	int min = 0; // для записи минимального значения
+	int buf = 0; // для обмена значениями 
+
+	/*********** Начало сортировки **************/
+	for (int i = 0; i < N; i++)
+	{
+		min = i; // запомним номер текущей ячейки, как ячейки с минимальным значением
+		// в цикле найдем реальный номер ячейки с минимальным значением
+		for (int j = i + 1; j < N; j++)
+			min = (a[j] < a[min]) ? j : min;
+		// cделаем перестановку этого элемента, поменяв его местами с текущим
+		if (i != min)
+		{
+			buf = a[i];
+			a[i] = a[min];
+			a[min] = buf;
 		}
 	}
-	return -1; // Искомый элемент не найден, возвращаем -1
-}
+	/*********** Конец сортировки **************/
 
-int main() {
-	setlocale(LC_ALL, "Russian");
-	vector <int> arr { 1, 2, 3, 5, 6, 7, 9 };
-	int size = sizeof(arr) / sizeof(arr[0]);
-	int key = 9;
-
-	int result = binarySearch(arr, 0, size - 1, key);
-	if (result != -1) {
-		std::cout << "Искомый элемент " << key << " найден по индексу " << result << std::endl;
-	}
-	else {
-		std::cout << "Искомый элемент " << key << " не найден." << std::endl;
-	}
-
-	return 0;
+	for (int i = 0; i < N; i++) //Вывод отсортированного массива
+		cout << a[i] << '\t';
+	cout << endl;
 }
